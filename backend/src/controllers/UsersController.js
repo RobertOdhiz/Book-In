a7517d8e53726bbb8b7971ddfad8d95c5b68ca7c
Brian <chumobrian@gmail.com>
2024-07-03 19:01:04 +0300

    feat: Add user controller endpoints to find user by id and delete users

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -1,4 +1,5 @@
 const dbClient = require('../utils/db');
+const { ObjectId } = require('mongodb');
 
 class UsersController {
     static async postUserRegister(req, res) {
@@ -12,12 +13,10 @@ class UsersController {
             return res.status(500).json({ error: 'Database server not connected' });
         }
 
-        const usersCollection = await dbClient.dbUsers();
-
-        const checkUser = await usersCollection.findOne({ email });
+        const checkUser = await dbClient.db.collection('users').findOne({ email });
 
         if (checkUser) {
-            return res.status(400).json({ error: 'User already exists' });
+            return res.status(409).json({ error: 'User already exists' });
         }
 
         const user = {
@@ -27,27 +26,48 @@ class UsersController {
             organization
         };
 
-        const result = await usersCollection.insertOne(user);
+        const result = await dbClient.db.collection('users').insertOne(user);
 
         return res.status(201).json({ email, userName, id: result.insertedId });
     }
 
 
-    static async getMe(req, res) {
-        const { email } = req.body;
+    static async getUser(req, res) {
+        const { id } = req.params;
 
-        if (!email) {
+        if (!id) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
-        const user = await dbClient.dbUsers().findOne({ email });
+        const user = await dbClient.db.collection('users').findOne({ _id: ObjectId.createFromHexString(id) });
 
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
         return res.status(200).json({ user });
     }
-}
 
+    static async getUsers(req, res) {
+        if (dbClient.isAlive() === false) {
+            return res.status(500).json({ error: 'Database server not connected' });
+        }
+        const users = await dbClient.db.collection('users').find({}).toArray();
+        return res.status(200).json({ users });
+    }
+
+    static async deleteUser(req, res) {
+        const { id } = req.params;
+
+        if (!dbClient.isAlive()) {
+            return res.status(500).json({ error: 'Database server not connected' });
+        }
+        const user = await dbClient.db.collection('users').findOne({ _id: ObjectId.createFromHexString(id) });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        await dbClient.db.collection('users').deleteOne({ _id: ObjectId.createFromHexString(id) });
+        return res.status(200).json({ message: 'User deleted' });
+    }
+}
 module.exports = UsersController;
 
