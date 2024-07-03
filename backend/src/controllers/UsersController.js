c8bc4a53508a729bb079d74670e52619789c0f7a
Brian <chumobrian@gmail.com>
2024-07-03 12:40:59 +0300

    feat: Add users controller to manage user registration

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -2,43 +2,6 @@ const dbClient = require('../utils/db');
 
 class UsersController {
     static async postUserRegister(req, res) {
-        return res.status(200).json({ data: req.body });
-
-        // const { email, userName, userId, organization } = req.body;
-
-        // if (!req.body) {
-        //     return res.status(400).json({ error: 'Invalid input' });
-        // }
-
-        // if (!dbClient.isAlive()) {
-        //     return res.status(500).json({ error: 'Database server not connected' });
-        // }
-
-        // const usersCollection = await dbClient.dbUsers();
-
-        // const checkUser = await usersCollection.findOne({ email });
-
-        // if (checkUser) {
-        //     return res.status(400).json({ error: 'User already exists' });
-        // }
-
-        // const user = {
-        //     email,
-        //     name: userName,
-        //     userId,
-        //     organization
-        // };
-
-        // const result = await usersCollection.insertOne(user);
-
-        // return res.status(201).json({ email, userName, id: result.insertedId });
-    }
-
-    static async getMe(req, res) {
-        return res.status(200).json({ success: 'Test Success' });
-    }
-
-    static async postUserLogin(req, res) {
         const { email, userName, userId, organization } = req.body;
 
         if (!req.body) {
@@ -67,9 +30,24 @@ class UsersController {
         const result = await usersCollection.insertOne(user);
 
         return res.status(201).json({ email, userName, id: result.insertedId });
+    }
 
-        //return res.status(200).json({ data: req.body });
+
+    static async getMe(req, res) {
+        const { email } = req.body;
+
+        if (!email) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const user = await dbClient.dbUsers().findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        return res.status(200).json({ user });
     }
 }
 
 module.exports = UsersController;
+
