deb5e3fcdc03485fb4274b8593bb1273efb30fb2
Brian <chumobrian@gmail.com>
2024-07-03 12:43:04 +0300

    feat: Add users and events routes to manage HTTP requests

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,26 +1,19 @@
 import express from 'express';
-const UsersController = require('../controllers/UsersController');
-const EventsController = require('../controllers/EventsController');
+import UsersController from '../controllers/UsersController';
+import EventsController from '../controllers/EventsController';
 
 const router = express.Router();
 
-/* User Authentication and Management */
+/* User Registration */
 router.post('/register', UsersController.postUserRegister);
-router.post('/login', UsersController.postUserLogin);
-// router.post('/logout', UsersController.postUserLogout);
-// router.get('/profile', UsersController.getUserProfile);
-// router.put('/profile', UsersController.updateUserProfile);
+router.post('/get-me', UsersController.getMe);
 
-/* Event Management */
-// router.post('/events', EventsController.postCreateEvent);
-// router.get('/events', EventsController.getEvents);
-// router.get('/events/:event_id', EventsController.getEventById);
-// router.put('/events/:event_id', EventsController.updateEventById);
-// router.delete('/events/:event_id', EventsController.deleteEventById);
-
-/* Event Registration */
-// router.post('/events/:event_id/register', EventsController.registerUserForEvent);
-// router.delete('/events/:event_id/register', EventsController.unregisterUserFromEvent);
-// router.get('/events/:event_id/attendees', EventsController.getEventAttendees);
+/* Event CRUD */
+router.get('/events', EventsController.getEvents);
+router.get('/events/:id', EventsController.getEventById);
+router.post('/events', EventsController.postCreateEvent);
+router.put('/events/:id', EventsController.updateEventById);
+router.delete('/events/:id', EventsController.deleteEventById);
 
 module.exports = router;
+
