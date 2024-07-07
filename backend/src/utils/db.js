f513fb4caf6da43e937f5a14879271db56101c6c
Robert Odhiambo <odhizrober254@gmail.com>
2024-07-07 23:51:43 +0300

    Error handling in db.js

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -7,15 +7,15 @@ class DBClient {
         console.log(DB_HOST);
         //const url = `mongodb://${DB_HOST}:${DB_PORT}`;
         const uri = DB_ATLAS;
-        const url = uri;
-        this.client = new MongoClient(url);
+        // const url = uri;
+        this.client = new MongoClient(uri);
 
         this.client.connect()
             .then(() => {
                 this.db = this.client.db(DB_DATABASE)
                 console.log('Connection to MongoDB established');
             })
-            .catch(console.log('Error connecting to MongoDB:'));
+            .catch(err => console.error(`Error connecting to DB: ${err.name} with reason of type ${err.reason.type}`));
     }
     isAlive() {
         return this.client && this.client.topology && this.client.topology.isConnected();
