d248795155dad6164301202932ab48a8d6c5b0b3
Robert Odhiambo <odhizrober254@gmail.com>
2024-07-09 18:09:37 +0300

    Updated vercel.json and refactored server.js for Vercel deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,4 +10,6 @@ app.use(urlencoded({ extended: true }));
 app.use('/', routes);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => { console.log(`app listening on port ${port}`) })
+app.listen(port, () => { console.log(`app listening on port ${port}`) });
+
+module.exports = app;
